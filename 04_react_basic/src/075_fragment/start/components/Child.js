15c4830a1c513e91b814703c8da4e50683929128
hasegawa2073 <hasetatsu2001@icloud.com>
2022-08-11 12:18:08 +0900

    Add: フラグメントを使った

--- 04_react_basic/src/075_fragment/start/components/Child.js
+++ 04_react_basic/src/075_fragment/start/components/Child.js
@@ -1,10 +1,20 @@
-import "./Child.css";
+import './Child.css';
+import React from 'react';
 
 const Child = () => {
   return (
-    <div className="component">
-      <h3>Hello Component</h3>
-    </div>
+    <React.Fragment>
+      <div className="component">
+        <h3>Hello Component</h3>
+      </div>
+      <h3>Hello Fragment</h3>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptate
+        dolores officiis accusamus quis, tempora odit vel impedit nihil
+        aspernatur autem qui mollitia quam quas atque vitae placeat fuga laborum
+        temporibus.
+      </p>
+    </React.Fragment>
   );
 };
 
