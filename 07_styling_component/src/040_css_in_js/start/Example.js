f9383d3a4204cf1f45bb5a50844252ccb9cc1ee8
hasegawa2073 <hasetatsu2001@icloud.com>
2022-08-17 13:37:23 +0900

    Add: styled-componentsを使ってみた

--- 07_styling_component/src/040_css_in_js/start/Example.js
+++ 07_styling_component/src/040_css_in_js/start/Example.js
@@ -1,4 +1,35 @@
-import { useState } from "react";
+import { useState } from 'react';
+import styled from 'styled-components';
+
+console.dir(styled);
+const StyledButton = styled.button`
+  margin: auto;
+  border-radius: 9999px;
+  border: none;
+  display: block;
+  width: 120px;
+  height: 60px;
+  font-weight: bold;
+  cursor: pointer;
+  background: ${({ isSelected }) => (isSelected ? 'pink' : '')};
+
+  @media (max-width: 600px) {
+    border-radius: 0;
+  }
+`;
+
+const OrangeButton = styled(StyledButton)`
+  background: orange;
+
+  :hover {
+    color: green;
+    opacity: 0.7;
+  }
+
+  span {
+    font-size: 1.2em;
+  }
+`;
 
 const Example = () => {
   const [isSelected, setIsSelected] = useState(false);
@@ -7,11 +38,20 @@ const Example = () => {
 
   return (
     <>
-      <button className={`btn ${isSelected ? "selected" : ""}`} onClick={clickHandler}>
+      <StyledButton isSelected={isSelected} onClick={clickHandler}>
+        ボタン
+      </StyledButton>
+      <OrangeButton isSelected={isSelected} onClick={clickHandler}>
+        <span>ボタン</span>
+      </OrangeButton>
+      <button
+        className={`btn ${isSelected ? 'selected' : ''}`}
+        onClick={clickHandler}
+      >
         ボタン
       </button>
-      <div style={{ textAlign: "center" }}>
-        {isSelected && "クリックされました。"}
+      <div style={{ textAlign: 'center' }}>
+        {isSelected && 'クリックされました。'}
       </div>
     </>
   );
