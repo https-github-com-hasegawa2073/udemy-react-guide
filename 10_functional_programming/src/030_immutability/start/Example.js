12cb7745c4eefcab87a09920587b093b424e6916
hasegawa2073 <hasetatsu2001@icloud.com>
2022-10-15 22:41:24 +0900

    add: 引数で渡された値を変更しないことをImmutablityの保持ということを学んだ

--- 10_functional_programming/src/030_immutability/start/Example.js
+++ 10_functional_programming/src/030_immutability/start/Example.js
@@ -1,4 +1,11 @@
 const Example = () => {
+  const num = { val: 2 };
+  const double = (num) => {
+    const newNum = {};
+    newNum.val = num.val * 2;
+    return newNum;
+  };
+  console.log(double(num));
   return (
     <>
       <h3>不変性（Immutability）</h3>
