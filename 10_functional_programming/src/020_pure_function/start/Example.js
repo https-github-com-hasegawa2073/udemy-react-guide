c56b904a89e58ef047ffb7818153b1817981824f
hasegawa2073 <hasetatsu2001@icloud.com>
2022-10-15 22:21:39 +0900

    add: 純粋関数、決まった引数で決まった戻り値を返す

--- 10_functional_programming/src/020_pure_function/start/Example.js
+++ 10_functional_programming/src/020_pure_function/start/Example.js
@@ -1,7 +1,13 @@
 const Example = () => {
+  const val1 = 3,
+    val2 = 4;
+  const add = (val1, val2) => {
+    return val1 + val2;
+  };
+
   return (
     <>
-      <h3>純粋関数</h3>
+      <h3>純粋関数:{add(val1, val2)}</h3>
       <p>fn(決まった引数) には 決まった戻り値 を返す</p>
     </>
   );
