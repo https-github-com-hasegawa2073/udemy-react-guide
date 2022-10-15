dae67f2b9554108e2b8d8b8c5a325e8e1297df81
hasegawa2073 <hasetatsu2001@icloud.com>
2022-10-15 22:13:51 +0900

    add: 関数型は状態と処理を分離して管理すると学んだ

--- 10_functional_programming/src/010_data_procedure/start/Example.js
+++ 10_functional_programming/src/010_data_procedure/start/Example.js
@@ -1,8 +1,25 @@
 const Example = () => {
+  // 関数型
+  const nums = [1, 2, 3];
+  const sum = (arry) => arry.reduce((accu, curr) => accu + curr);
+
+  // オブジェクト指向型
+  const numObj = {
+    nums: [1, 2, 3],
+    sum() {
+      const nums = this.nums;
+      let sumValue = 0;
+      for (let i = 0; i < nums.length; i++) {
+        sumValue += nums[i];
+      }
+      return sumValue;
+    },
+  };
+
   return (
     <>
-      <h3>状態管理と処理を分離</h3>
-      <p>状態（データ）と処理（やりたいこと）は切り離す</p>
+      <div>オブジェクト指向型:{numObj.sum()}</div>
+      <p>関数型:{sum(nums)}</p>
     </>
   );
 };
