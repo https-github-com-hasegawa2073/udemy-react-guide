90aeaf9b350d74ea1d14843b004d5b3af4e5a67c
hasegawa2073 <hasetatsu2001@icloud.com>
2022-10-17 22:40:20 +0900

    add: Reactにおける不変性を学んだ

--- 10_functional_programming/src/060_react_immutability/start/Example.js
+++ 10_functional_programming/src/060_react_immutability/start/Example.js
@@ -1,20 +1,36 @@
-import { useState } from "react";
+import { useState } from 'react';
 
-const Child = (props) => {
+// POINT ReactでのImmutability
+// 関数型（純粋関数）
+// ・fn(決まった引数) -> 決まった戻り値
+// ・関数外の状態（データ）は参照・変更しない。
+// ・関数外に影響を及ぼさない。
+// ・引数で渡された値を変更しない。（★Immutability）
+const Child = ({ state, setState }) => {
+  // setState({ value: 1 })
+  // props.state = { value: 1 }
+  const increment = () => {
+    setState((prev) => {
+      const newState = { value: prev.value + 1 };
+      // prev.value += 1;
+      return newState;
+    });
+  };
   return (
     <>
-      <span>{props.state.value}</span>
+      <span>{state.value}</span>
+      <button onClick={increment}>+</button>
     </>
   );
 };
 
 const Example = () => {
-  const [ state, setState ] = useState({ value: 0 });
+  const [state, setState] = useState({ value: 0 });
 
   return (
     <>
       <div>
-        <Child state={state}/>
+        <Child state={state} setState={setState} />
       </div>
     </>
   );
