fe741cdb8f46ce67616bdb470c202b1b265c6732
hasegawa2073 <hasetatsu2001@icloud.com>
2022-10-17 22:09:41 +0900

    add: Reactのコンポーネントは純粋関数である必要があると学んだ

--- 10_functional_programming/src/040_react_pure_fn/start/Example.js
+++ 10_functional_programming/src/040_react_pure_fn/start/Example.js
@@ -1,7 +1,14 @@
-const Example = () => {
+const ChildPure = ({ value }) => {
+  return <div>{value}</div>;
+};
 
+const Example = () => {
+  let value = 0;
   return (
     <>
+      <ChildPure value={++value} />
+      <ChildPure value={++value} />
+      <ChildPure value={++value} />
     </>
   );
 };
