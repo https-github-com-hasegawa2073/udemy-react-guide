0b6fe82f6f4a5132a798189156a469c0499843d5
hasegawa2073 <hasetatsu2001@icloud.com>
2022-10-18 17:25:21 +0900

    add: コンテキストのProviderは分けて書くとよいと学んだ

--- 11_hooks_p1/src/050_context_file/start/components/Header.js
+++ 11_hooks_p1/src/050_context_file/start/components/Header.js
@@ -1,5 +1,27 @@
+import { useTheme } from '../context/ThemeContext';
+
 const Header = () => {
-  
+  const [theme, setTheme] = useTheme();
+  const THEMES = ['light', 'dark', 'red'];
+  const changeTheme = (e) => setTheme(e.target.value);
+  return (
+    <header className={`content-${theme}`}>
+      {THEMES.map((_theme) => {
+        return (
+          <label key={_theme}>
+            <input
+              key={_theme}
+              value={_theme}
+              checked={theme === _theme}
+              type="radio"
+              onChange={changeTheme}
+            ></input>
+            {_theme}
+          </label>
+        );
+      })}
+    </header>
+  );
 };
 
 export default Header;
