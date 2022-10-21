5a4dccee41a14efcc3e801b1464d2e581a08c6f1
hasegawa2073 <hasetatsu2001@icloud.com>
2022-10-21 17:27:39 +0900

    add: useLayoutEffectはuseEffectより先に実行されると学んだ

--- 12_hooks_p2/src/050_useLayoutEffect/start/Example.js
+++ 12_hooks_p2/src/050_useLayoutEffect/start/Example.js
@@ -1,14 +1,16 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useLayoutEffect } from 'react';
+import Random from './Random';
+
 const Example = () => {
   const [isDisp, setIsDisp] = useState(true);
 
   return (
     <>
-      {isDisp && <Timer/>}
-      <button onClick={() => setIsDisp(prev => !prev)}>トグル</button>
+      {isDisp && <Timer />}
+      <button onClick={() => setIsDisp((prev) => !prev)}>トグル</button>
     </>
-  )
-}
+  );
+};
 const Timer = () => {
   const [time, setTime] = useState(0);
 
@@ -17,31 +19,40 @@ const Timer = () => {
     let intervalId = null;
     intervalId = window.setInterval(() => {
       console.log('interval called');
-      setTime(prev => prev + 1);
+      setTime((prev) => prev + 1);
     }, 1000);
     return () => {
-      window.clearInterval(intervalId)
+      window.clearInterval(intervalId);
       // console.log('end');
-    }
-  }, [])
-  
+    };
+  }, []);
+
   useEffect(() => {
     // console.log('updated');
-    
+
     document.title = 'counter:' + time;
     window.localStorage.setItem('time-key', time);
 
     return () => {
       // console.log('updated end');
-    }
+    };
   }, [time]);
+
+  useLayoutEffect(() => {
+    const _time = parseInt(window.localStorage.getItem('time-key'));
+    if (!isNaN(_time)) {
+      setTime(_time);
+    }
+    return () => {};
+  }, []);
 
   return (
     <h3>
       <time>{time}</time>
       <span>秒経過</span>
+      <Random />
     </h3>
-    );
+  );
 };
 
 export default Example;
