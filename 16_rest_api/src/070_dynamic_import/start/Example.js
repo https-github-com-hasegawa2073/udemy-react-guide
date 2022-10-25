17d62e6192d9e9694e5f9d5fe1eab8a99c33d73e
hasegawa2073 <hasetatsu2001@icloud.com>
2022-10-25 21:37:30 +0900

    add: 動的インポートについて確認した

--- 16_rest_api/src/070_dynamic_import/start/Example.js
+++ 16_rest_api/src/070_dynamic_import/start/Example.js
@@ -1,5 +1,12 @@
+import { add } from './add';
+
 const Example = () => {
-  
-};
+  const dynamicImport = async () => {
+    const module = await import('./add');
+    console.log(module);
+  };
+  dynamicImport();
+  // console.log(add(1, 2));
+};;
 
 export default Example;
