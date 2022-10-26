52d5be4d09271c572e51b591bcd4b41fb7fb282c
hasegawa2073 <hasetatsu2001@icloud.com>
2022-10-26 14:54:24 +0900

    add: getServerSideProps関数を触ってみた

--- 18_nextjs_p2/start/src/pages/010_SSR/index.js
+++ 18_nextjs_p2/start/src/pages/010_SSR/index.js
@@ -2,23 +2,27 @@
 import { useEffect, useState } from 'react';
 
 // export default function SSR({message}) {
-export default function SSR() {
-  console.log('hello');
+export default function SSR({ message }) {
+  //   console.log('hello');
+  console.log(message);
+
+  useEffect(() => {
+    console.log('useEffect');
+    document.cookie = 'val=0; path=/';
+  });
   const [state, setState] = useState('bye');
   const val = 0;
   return <h3>{state}</h3>;
 }
 
-// export async function getServerSideProps(context) {
-
-//     const { cookie } = context.req.headers;
-//     console.log('cookie', cookie);
-//     console.log('getServerSideProps is executed')
-//     return {
-//         // redirect: {
-//         //     destination: '/',
-//         //     permanent: false
-//         // },
-//         props: { message: 'From Server Side Props' }
-//     }
-// }
+export async function getServerSideProps(context) {
+  const { cookie } = context.req.headers;
+  console.log('cookie: ', cookie);
+  return {
+    // redirect: {
+    //   destination: '/',
+    //   permanent: true,
+    // },
+    props: { message: 'From Server Side Props' },
+  };
+}
