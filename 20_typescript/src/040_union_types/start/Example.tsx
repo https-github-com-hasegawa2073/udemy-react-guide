7d36e37e0f4e69bb92e7397880d712664809b199
hasegawa2073 <hasetatsu2001@icloud.com>
2022-10-27 14:23:06 +0900

    add: ユニオン型の定義について確認した

--- 20_typescript/src/040_union_types/start/Example.tsx
+++ 20_typescript/src/040_union_types/start/Example.tsx
@@ -1,5 +1,13 @@
 const Example = () => {
   // 練習用
+  let strOrNum: string | number = 'Hello';
+  strOrNum = 123;
+  console.log(strOrNum);
+
+  type helloOrNum = 'Hello' | number;
+  const hello: helloOrNum = 'Hello';
+  console.log(hello);
+  
 };
 
 export default Example;
