e628a94a6945e5a1594027483b2ceb6c545c3ac2
hasegawa2073 <hasetatsu2001@icloud.com>
2022-10-29 22:03:50 +0900

    add: 非同期処理をテストした

--- 19_test/src/060_async_test/start/components/GetUserData.test.js
+++ 19_test/src/060_async_test/start/components/GetUserData.test.js
@@ -1,4 +1,11 @@
 import { render, screen } from "@testing-library/react";
 import GetUserData from "./GetUserData";
+import { ENDPOINT_URL } from '../Example';
 
-test('ダミーテスト:削除して記載してください。', () => {});
+describe('GetUserDataコンポーネントの動作確認', () => {
+  test('外部データ取得中', async () => {
+    render(<GetUserData url={ENDPOINT_URL} />);
+    const h2El = await screen.findByRole('heading', { name: 'プロフィール' });
+    expect(h2El).toBeInTheDocument();
+  });
+});
